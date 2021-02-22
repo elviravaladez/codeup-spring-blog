a430247938f909725da6fcb1680de44548ec98bf
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-02-22 17:13:57 -0600

    feat: user email on show.html and assign user to post BEFORE post is saved to db

---
+++ src/main/java/com/spring/springblog/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.spring.springblog.repositories;
+
+import com.spring.springblog.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
