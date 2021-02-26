8f91802f4836a1f8152edcedc6a9c1bbf3062d07
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-02-26 09:46:56 -0600

    update: User class

--- src/main/java/com/spring/springblog/models/User.java
+++ src/main/java/com/spring/springblog/models/User.java
@@ -31,10 +31,19 @@ public class User {
     //constructors
     public User() { }
 
-    public User(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
+//    public User(String username, String email, String password) {
+//        this.username = username;
+//        this.email = email;
+//        this.password = password;
+//    }
+
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+        //The line below was not initially a part of the constructor
+        posts = copy.posts;
     }
 
     //getters and setters
---
+++ src/main/java/com/spring/springblog/services/UserDetailsLoader.java
@@ -0,0 +1,4 @@
+package com.spring.springblog.services;
+
+public class UserDetailsLoader {
+}
