12bb5fa3fff3d20539e651ee10685b87828d690f
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-03-04 12:23:46 -0600

    refactor: HelloController

--- src/main/java/com/spring/springblog/controllers/HelloController.java
+++ src/main/java/com/spring/springblog/controllers/HelloController.java
@@ -6,18 +6,18 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    @ResponseBody
-
-    public String hello(){
-        return "Hello from Spring!";
-    }
-
-    @GetMapping("/hello/{name}")
-    @ResponseBody
-    public String sayHello(@PathVariable String name){
-        return "Hello " + name + "!";
-    }
+//    @GetMapping("/hello")
+//    @ResponseBody
+//
+//    public String hello(){
+//        return "Hello from Spring!";
+//    }
+//
+//    @GetMapping("/hello/{name}")
+//    @ResponseBody
+//    public String sayHello(@PathVariable String name){
+//        return "Hello " + name + "!";
+//    }
 
     @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
     @ResponseBody
