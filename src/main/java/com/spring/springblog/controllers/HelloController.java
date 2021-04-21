7223b407a01e56b2a15884442c16dbcc7ae7110a
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-04-21 17:53:09 -0500

    remove: commented out code

--- src/main/java/com/spring/springblog/controllers/HelloController.java
+++ src/main/java/com/spring/springblog/controllers/HelloController.java
@@ -5,20 +5,6 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
-
-//    @GetMapping("/hello")
-//    @ResponseBody
-//
-//    public String hello(){
-//        return "Hello from Spring!";
-//    }
-//
-//    @GetMapping("/hello/{name}")
-//    @ResponseBody
-//    public String sayHello(@PathVariable String name){
-//        return "Hello " + name + "!";
-//    }
-
     @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
     @ResponseBody
     public String addOne(@PathVariable int number){
